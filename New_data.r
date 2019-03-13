e891fc7c6d1af5b93743b34e2eebcf04d49fabe8
Noah Spahn <noah-de@users.noreply.github.com>
2019-03-12 21:26:41 -0700

    Remove bad line, Closes #1
    https://help.github.com/en/articles/closing-issues-using-keywords

--- New_data.r
+++ New_data.r
@@ -6,6 +6,5 @@ new_data <- read.csv(here("raw_data", iris_mythica.csv))
 # Put them together
 data <- rbind(old_data, new_data)
 
-
 # Save them
 write.csv(x = data, file = here("data", "iris_four.csv"))
